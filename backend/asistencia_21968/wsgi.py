fb83dd72a1dfc3d0c709747ac029da8f2ed3b037
Crowdbotics <team@crowdbotics.com>
2020-10-26 14:23:50 +0000

    Update models

--- backend/asistencia_21968/wsgi.py
+++ backend/asistencia_21968/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asistencia_21968.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asistencia_21968.settings")
 
 application = get_wsgi_application()
